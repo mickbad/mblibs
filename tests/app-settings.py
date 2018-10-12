8da12244add3b2009a35617bb8dff47d93cbd594
Mick <31075343+mickbad@users.noreply.github.com>
2018-10-12 12:40:44 +0200

    Update app-settings.py

--- tests/app-settings.py
+++ tests/app-settings.py
@@ -90,8 +90,8 @@ if __name__ == '__main__':
 
     # démarrage du programme
     try:
-        main(config_pathname)
         print("mblibs testing settings (v{})".format(mblibs.__version__))
+        main(config_pathname)
 
     except Exception as e:
         print("\n\n\n** Something went wrong:\n{}".format(str(e)))
